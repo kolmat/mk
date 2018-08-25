3ce057e079da5504a8a6d653d4c8252b6a01ce6e
root <root@root.com>
2018-08-25 13:49:36 +0200

    minor fix

--- removable.sh
+++ removable.sh
@@ -2,13 +2,13 @@
 
 #it will have always one element ""
 removable=($(lsblk -o MOUNTPOINT,RM |egrep "[1]$" | awk ' $1 != 1   { print $1}') "")
-
+option="nodev"
 printf '%s\n' "${removable[@]}"
 
 if [[ ${#removable[@]} -gt 1 ]]; then
 	for dev in ${removable[@]}; do
-		echo "${dev} is USB"
-		if [[ -z $(findmnt -o OPTIONS ${dev}| grep nodev) ]]; then 
+		echo "${dev} is removable"
+		if [[ -z $(findmnt -o OPTIONS ${dev}| grep -w ${option}) ]]; then 
 			echo "FAIL"
 			exit 1
 		fi 
